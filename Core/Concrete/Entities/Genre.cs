c957a4d90fe3c47abf0af75d71aff80a456a8378
aribilgiogr <aribilgiogr@gmail.com>
2026-07-11 10:11:52 +0300

    Update 01 11.07.2026

--- Core/Concrete/Entities/Genre.cs
+++ Core/Concrete/Entities/Genre.cs
@@ -4,7 +4,7 @@ namespace Core.Concrete.Entities
 {
     public class Genre : BaseEntity
     {
-        public required string Name { get; set; }
+        public required string Name { get; set; } // Örn: Drama|Fantasy|Horror|Mystery|Thriller veya Comedy
         public virtual ICollection<Book> Books { get; set; } = [];
     }
 }
